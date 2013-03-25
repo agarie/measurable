21a646b6e8f6225b62977ba0e2a95c2c11b87637
Carlos Agarie <carlos.agarie@gmail.com>
2013-03-24 17:01:54 -0700

    Updated specs and the euclidean distance
    - Added support for calculating norms when given a single argument.
    - Raises an exception when given two Arrays of different sizes.
    
    The specs cover most of the functionality of Measurable#euclidean and
    Measurable#euclidean_squared. Now I need to cover the other metrics!

--- lib/measurable/euclidean.rb
+++ lib/measurable/euclidean.rb
@@ -1,16 +1,37 @@
 module Measurable
   class << self
-    def euclidean(u, v)
-      sum = 0.0
-    
-      u.zip(v).each do |ary|
-        sum += (ary[0] - ary[-1])**2
+    # Add documentation here!
+    def euclidean(u, v = nil)
+      # If the second argument is nil, the method should return the norm of
+      # vector u. For this, we need the distance between u and the origin.
+      if v.nil?
+        v = Array.new(u.size, 0)
+      end
+      
+      # We could make it work with vector of different sizes because of #zip
+      # but it's unreliable. It's better to just throw an exception.
+      # TODO: Change this to a more specific, custom-made exception.
+      raise ArgumentError if u.size != v.size
+      
+      sum = u.zip(v).reduce(0.0) do |acc, ary|
+        acc += (ary[0] - ary[-1])**2
       end
     
       Math.sqrt(sum)
     end
   
-    def euclidean_squared(u, v)
+    def euclidean_squared(u, v = nil)
+      # If the second argument is nil, the method should return the norm of
+      # vector u. For this, we need the distance between u and the origin.
+      if v.nil?
+        v = Array.new(u.size, 0)
+      end
+      
+      # We could make it work with vector of different sizes because of #zip
+      # but it's unreliable. It's better to just throw an exception.
+      # TODO: Change this to a more specific, custom-made exception.
+      raise ArgumentError if u.size != v.size
+      
       u.zip(v).reduce(0.0) do |acc, ary|
         acc += (ary[0] - ary[-1])**2
       end
--- spec/measurable_spec.rb
+++ spec/measurable_spec.rb
@@ -1,9 +1,5 @@
 describe Measurable do
 
-  let(:u) { [1, 3, 16] }
-  let(:v) { [1, 4, 16] }
-  let(:w) { [4, 5, 6] }
-
   describe "Binary union" do
     
   end
@@ -12,25 +8,67 @@ describe Measurable do
     
   end
 
-  describe "Euclidean distance" do
-    it "accepts two arguments" do
-      expect { Measurable.euclidean(:u) }.to raise_error(ArgumentError)
-      expect { Measurable.euclidean(:u, :v) }.to_not raise_error(ArgumentError)
-      expect { Measurable.euclidean(:u, :v, :w) }.to raise_error(ArgumentError)
+  describe "Euclidean" do
+    
+    before :all do
+      @u = [1, 3, 16]
+      @v = [1, 4, 16]
+      @w = [4, 5, 6]
     end
     
-    it "accepts one argument and returns the vector's norm"
+    context "Distance" do
+      it "accepts two arguments" do
+        expect { Measurable.euclidean(@u, @v) }.to_not raise_error
+        expect { Measurable.euclidean(@u, @v, @w) }.to raise_error(ArgumentError)
+      end
+    
+      it "accepts one argument and returns the vector's norm" do
+        # Remember that 3^2 + 4^2 = 5^2.
+        Measurable.euclidean([3, 4]).should == 5
+      end
             
-    it "should be symmetric"
+      it "should be symmetric" do
+        Measurable.euclidean(@u, @v).should == Measurable.euclidean(@v, @u)
+      end
 
-    it "should return the correct value" do
-      Measurable::euclidean(:u, :u).should == 0
-      euclidean(:u, :v).should == 1
+      it "should return the correct value" do
+        Measurable.euclidean(@u, @u).should == 0
+        Measurable.euclidean(@u, @v).should == 1
+      end
+    
+      it "shouldn't work with vectors of different length" do
+        expect { Measurable.euclidean(@u, [2, 2, 2, 2]) }.to raise_error
+      end
     end
     
-    it "shouldn't work with vectors of different length" do
-      expect { Measurable::euclidean(:u, [2, 2, 2, 2]) }.to raise_error
+    context "Squared Distance" do
+      it "accepts two arguments" do
+        expect { Measurable.euclidean_squared(@u, @v) }.to_not raise_error
+        expect { Measurable.euclidean_squared(@u, @v, @w) }.to raise_error(ArgumentError)
+      end
+    
+      it "accepts one argument and returns the vector's norm" do
+        # Remember that 3^2 + 4^2 = 5^2.
+        Measurable.euclidean_squared([3, 4]).should == 25
+      end
+            
+      it "should be symmetric" do
+        x = Measurable.euclidean_squared(@u, @v)
+        y = Measurable.euclidean_squared(@v, @u)
+        
+        x.should == y
+      end
+
+      it "should return the correct value" do
+        Measurable.euclidean_squared(@u, @u).should == 0
+        Measurable.euclidean_squared(@u, @v).should == 1
+      end
+    
+      it "shouldn't work with vectors of different length" do
+        expect { Measurable.euclidean_squared(@u, [2, 2, 2, 2]) }.to raise_error
+      end    
     end
+    
   end
 
   describe "Cosine distance" do
