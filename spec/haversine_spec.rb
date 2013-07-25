483b95a9d96382c26d4ef458936095c979dbdc60
Carlos Agarie <carlos.agarie@gmail.com>
2013-07-24 23:32:07 -0300

    Updating specs
    Finished pending specs and added "better" input vectors for each method's
    specs (e.g. real data for #haversine, binary arrays for #tanimoto).

--- spec/haversine_spec.rb
+++ spec/haversine_spec.rb
@@ -1,25 +1,36 @@
 describe "Haversine distance" do
-  
+
   before :all do
-    @u = [1, 3]
-    @v = [1, 4]
-    @w = [4, 5]
+    # We have very big errors in this formula, due to:
+    #   - The Earth is considered a sphere.
+    #   - Earth's radius is considered constant (same as above).
+    #
+    # Given these conditions, I'll just assume the error to be less than 1.
+    # TODO: Calculate better error estimates.
+    @haversine_tolerance = 1
+
+    @u = [ 35.66667, 139.75] # Tokyo: 35 40' N, 139 45' E.
+    @v = [-23.53333, -46.61667] # São Paulo: 23 32' S, 46 37' W.
   end
-  
+
   it "accepts two arguments" do
     expect { Measurable.haversine(@u, @v) }.to_not raise_error
-    expect { Measurable.haversine(@u, @v, @w) }.to raise_error(ArgumentError)
+    expect { Measurable.haversine(@u, @v, [-24.5, 40.23]) }.to raise_error(ArgumentError)
   end
-  
+
   it "should be symmetric" do
     x = Measurable.haversine(@u, @v)
     y = Measurable.haversine(@v, @u)
-    
+
     x.should be_within(TOLERANCE).of(y)
   end
 
-  it "should return the correct value"
-  
+  it "should return the correct value" do
+    x = Measurable.haversine(@u, @v, :km)
+
+    x.should be_within(@haversine_tolerance).of(18533)
+  end
+
   it "should only work with [lat, long] vectors" do
     expect { Measurable.haversine([2, 4], [1, 3, 5, 7]) }.to raise_error
   end
--- spec/tanimoto_spec.rb
+++ spec/tanimoto_spec.rb
@@ -1,25 +1,29 @@
 describe "Tanimoto distance" do
-  
+
   before :all do
-    @u = [1, 3, 16]
-    @v = [1, 4, 16]
-    @w = [4, 5, 6]
+    @u = [1, 0, 1]
+    @v = [1, 1, 1]
+    @w = [0, 1, 0]
   end
-  
+
   it "accepts two arguments" do
     expect { Measurable.tanimoto(@u, @v) }.to_not raise_error
     expect { Measurable.tanimoto(@u, @v, @w) }.to raise_error(ArgumentError)
   end
-          
+
   it "should be symmetric" do
     x = Measurable.tanimoto(@u, @v)
     y = Measurable.tanimoto(@v, @u)
-    
+
     x.should be_within(TOLERANCE).of(y)
   end
 
-  it "should return the correct value"
-  
+  it "should return the correct value" do
+    x = Measurable.tanimoto(@u, @v)
+
+    x.should be_within(TOLERANCE).of(-Math.log2(2.0 / 3.0))
+  end
+
   it "shouldn't work with vectors of different length" do
     expect { Measurable.tanimoto(@u, [1, 3, 5, 7]) }.to raise_error
   end
