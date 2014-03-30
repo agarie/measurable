2fa4071d5f2f33fadc375f8783311463ba3a439c
Ross Kaffenberger <rosskaff@gmail.com>
2014-03-30 08:48:21 -0400

    levenshtein spec contexts for arrays and strings
    levenshtein spec contexts for arrays and strings
    
    remove extra gems

--- spec/levenshtein_spec.rb
+++ spec/levenshtein_spec.rb
@@ -22,20 +22,56 @@ describe Measurable::Levenshtein do
   # assert func(t("ab"), t("aa")) == 1
   # assert func(t("ab"), t("a")) == 1
   # assert func(t("ab"), t("abc")) == 1
+  context "strings" do
 
-  it "handles empty strings" do
-    Measurable.levenshtein("", "").should == 0
-    Measurable.levenshtein("", "abcd").should == 4
-    Measurable.levenshtein("abcd", "").should == 4
+    it "handles empty" do
+      Measurable.levenshtein("", "").should == 0
+      Measurable.levenshtein("", "abcd").should == 4
+      Measurable.levenshtein("abcd", "").should == 4
+    end
+
+    it "should not count equality" do
+      Measurable.levenshtein("aa", "aa").should == 0
+    end
+
+    it "should count deletion" do
+      Measurable.levenshtein("ab", "a").should == 1
+    end
+
+    it "should count insertion" do
+      Measurable.levenshtein("ab", "abc").should == 1
+    end
+
+    it "should count substitution" do
+      Measurable.levenshtein("aa", "ab").should == 1
+    end
+
   end
 
-  it "should return 0 for matching strings" do
-    Measurable.levenshtein("aa", "aa").should == 0
-  end
+  context "arrays" do
 
-  it "should count insertion" do
-    #Measurable.levenshtein("ab", "a").should == 1
-    Measurable.levenshtein("ab", "abc").should == 1
+    it "handles empty" do
+      Measurable.levenshtein([], []).should == 0
+      Measurable.levenshtein([], %w[ a b c d ]).should == 4
+      Measurable.levenshtein(%w[ a b c d ], []).should == 4
+    end
+
+    it "should not count equality" do
+      Measurable.levenshtein(%w[ a ], %w[ a ]).should == 0
+    end
+
+    it "should count deletion" do
+      Measurable.levenshtein(%w[ a b ], %w[ a ]).should == 1
+    end
+
+    it "should count insertion" do
+      Measurable.levenshtein(%w[ a b ], %w[ a b c ]).should == 1
+    end
+
+    it "should count substitution" do
+      Measurable.levenshtein(%w[ a a ], %w[ a b ]).should == 1
+    end
+
   end
 
 end
