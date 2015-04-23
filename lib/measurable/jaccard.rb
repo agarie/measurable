c0af062f393ecab228aac36a3d047be478b1da7c
Carlos Agarie <carlos.agarie@gmail.com>
2015-04-23 14:58:50 -0300

    Change Jaccard naming

--- lib/measurable/jaccard.rb
+++ lib/measurable/jaccard.rb
@@ -29,18 +29,20 @@ module Measurable
       intersection.length.to_f / union.length
     end
 
+    alias_method :jaccard, :jaccard_index
+
     # call-seq:
-    #     jaccard(u, v) -> Float
+    #     jaccard_dissimilarity(u, v) -> Float
     #
     # The jaccard distance is a measure of dissimilarity between two sets. It is
     # calculated as:
     #   jaccard_distance = 1 - jaccard_index
     #
     # This is a proper metric, i.e. the following conditions hold:
-    #   - Symmetry:              jaccard(u, v) == jaccard(v, u)
-    #   - Non-negative:          jaccard(u, v) >= 0
-    #   - Coincidence axiom:     jaccard(u, v) == 0 if u == v
-    #   - Triangular inequality: jaccard(u, v) <= jaccard(u, w) + jaccard(w, v)
+    #   - Symmetry:              jaccard_dissimilarity(u, v) == jaccard(v, u)
+    #   - Non-negative:          jaccard_dissimilarity(u, v) >= 0
+    #   - Coincidence axiom:     jaccard_dissimilarity(u, v) == 0 if u == v
+    #   - Triangular inequality: jaccard_dissimilarity(u, v) <= jaccard(u, w) + jaccard(w, v)
     #
     # Arguments:
     # - +u+ -> Array.
@@ -49,9 +51,11 @@ module Measurable
     # - Float value representing the dissimilarity between +u+ and +v+.
     # Raises:
     # - +ArgumentError+ -> The size of the input arrays doesn't match.
-    def jaccard(u, v)
+    def jaccard_dissimilarity(u, v)
       1 - jaccard_index(u, v)
     end
+
+    alias_method :jaccard_distance, :jaccard_dissimilarity
 
     extend Measurable::Jaccard
   end
